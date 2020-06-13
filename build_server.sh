0fe3827d6d75358426240898807a32c06c925a46
Khai Phan <khai.phan@tiki.vn>
2020-06-13 19:00:03 +0700

    Update CI

--- build_server.sh
+++ build_server.sh
@@ -3,12 +3,16 @@ sudo chmod 777 /src
 cd /src
 wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb && sudo dpkg -i erlang-solutions_2.0_all.deb
 sudo apt-get update
-sudo apt-get install -y git elixir nodejs postgresql npm inotify-tools
+sudo apt-get install -y git elixir nodejs postgresql npm inotify-tools erlang-dev erlang-parsetools
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
 sudo service postgresql restart
 git clone https://github.com/pdkhai/yojee.git
 cd yojee/
-mix local.hex
-mix archive.install hex phx_new 1.5.3
+mix local.hex --force
+mix local.rebar --force
+mix archive.install hex phx_new 1.5.3 --force
 mix deps.get --force
+mix deps.update gettext --force
+mix ecto.create
+
 mix phx.server
