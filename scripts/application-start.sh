9e8d464933529582e9bd0c0d771e5330d5c6a0ad
Khai Phan <khai.phan@tiki.vn>
2020-06-14 22:09:49 +0700

    update

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-cd /tmp/yojee
+cd /tmp/yojee/assets
+npm install
+
+cd ..
+mix local.hex --force
+mix local.rebar --force
+mix archive.install hex phx_new 1.5.3 --force
+mix deps.get --force
+mix deps.update gettext --force
+mix ecto.create
 
 mix phx.server --force &
 
